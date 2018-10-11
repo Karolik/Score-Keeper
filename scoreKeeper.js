c6e47179f279829cef90911396e18af19a88f9ef
Karolina Gauvain <karolina.gauvain@gmail.com>
2018-10-11 21:27:22 +0200

    Add game over and max score functionality

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -7,19 +7,32 @@ const p2Display = document.querySelector("#p2Display");
 let p1Score = 0;
 let p2Score = 0;
 let gameOver = false;
+let maxScore = 5;
 
 //Click event for player 1 button
 p1Button.addEventListener("click", function(){
-  p1Score++;  //Increment the score
-  console.log(p1Score);
-  p1Display.textContent = p1Score;  //Display the score on the top
+  if(!gameOver){
+    p1Score++;  //Increment the score
+    
+    if(p1Score === maxScore) {
+      gameOver = true;
+      console.log("GAME OVER!");
+    }
+    p1Display.textContent = p1Score;  //Display the score on the top
+  }
 })
 
 //Click event for player 2 button
 p2Button.addEventListener("click", function(){
-  p2Score++; //Increment the score
-  console.log(p2Score);
-  p2Display.textContent = p2Score;  //Display the score on the top
+  if(!gameOver){
+    p2Score++;  //Increment the score
+    
+    if(p2Score === maxScore) {
+      gameOver = true;
+      console.log("GAME OVER!");
+    }
+    p2Display.textContent = p1Score;  //Display the score on the top
+  }
 })
 
 //Reset the game
