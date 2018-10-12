a0bcc41a31dbb068156edd67216cc6b7754b0d81
Karolina Gauvain <karolina.gauvain@gmail.com>
2018-10-12 23:02:06 +0200

    Add flexible winning score functionality
    Define winning score and numInput variables. Add the functionality to enable the user to change the winning score on the basis of the given number in the input field. Change the color of winner score to green.

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,9 +1,11 @@
 //Define variables
 const p1Button = document.querySelector(".p1");
 const p2Button = document.querySelector(".p2");
-const reset = document.querySelector(".reset");
+const resetButton = document.querySelector(".reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScore = document.querySelector(".winningScore");
+const numInput = document.querySelector(".inputNumber");
 let p1Score = 0;
 let p2Score = 0;
 let gameOver = false;
@@ -17,6 +19,7 @@ p1Button.addEventListener("click", function(){
     if(p1Score === maxScore) {
       gameOver = true;
       console.log("GAME OVER!");
+      p1Display.classList.add("winner"); //Change the color of the player score of the winner to green
     }
     p1Display.textContent = p1Score;  //Display the score on the top
   }
@@ -30,15 +33,28 @@ p2Button.addEventListener("click", function(){
     if(p2Score === maxScore) {
       gameOver = true;
       console.log("GAME OVER!");
+      p2Display.classList.add("winner");  //Change the color of the score of the winner to green
     }
     p2Display.textContent = p2Score;  //Display the score on the top
   }
 })
 
-//Reset the game
-reset.addEventListener("click", function(){
+//Reset the game when click
+resetButton.addEventListener("click", reset)
+
+//Function to reset the game
+function reset(){
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = p1Score;
   p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+  winningScore.textContent = this.value;
+  maxScore = Number(this.value);
+  reset();
 })
